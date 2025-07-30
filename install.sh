3e48a6f97df8b988a4a198660a069b6e91921d95
Koung <k.koungmyathein@student.maastrichtuniversity.nl>
2025-07-30 10:32:23 +0200

    Add sudo, apt update, and -y flag to install script for smoother execution

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+sudo apt update
+
 echo "Installing python venv..."
-apt install python3.12-venv
+sudo apt install -y python3.12-venv
 
 echo "Creating virtual environment..."
 python3 -m venv venv
@@ -16,14 +18,13 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Installing required Qt system libraries..."
-sudo apt update
 sudo apt install -y \
   libxcb-xinerama0 libxkbcommon-x11-0 libxcb1 libxcb-keysyms1 libxcb-image0 \
   libxcb-shm0 libxcb-icccm4 libxcb-sync1 libxcb-xfixes0 libxcb-shape0 libxcb-randr0 \
   libxcb-render-util0 libxrender1 libxkbcommon0 libgl1
 
 echo "Installing required tools..."
-sudo apt install libimage-exiftool-perl
+sudo apt install -y libimage-exiftool-perl
 
 echo "Installation complete. Run the app with:"
 echo "source venv/bin/activate && python main.py"
